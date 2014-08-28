cdca5e71c06495c88ea5c55709d2117ed89f3aac
Matias Franci-Home <mfranci@stockergroup.com>
2014-08-27 22:10:40 -0500

    Utilizando HttpURL Connection

--- app/src/main/java/com/example/matt/flowercatalog/HttpManager.java
+++ app/src/main/java/com/example/matt/flowercatalog/HttpManager.java
@@ -1,31 +1,46 @@
 package com.example.matt.flowercatalog;
 
-import android.net.http.AndroidHttpClient;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.util.EntityUtils;
 
-/**
- * Created by matt on 12/08/2014.
- */
 public class HttpManager {
-   public static String getData(String uri){
-      AndroidHttpClient client = AndroidHttpClient.newInstance("AndroidAgent");
 
-      //defino el target del request
-      HttpGet request = new HttpGet(uri);
-      //instancio objeto response
-      HttpResponse response;
+   public static String getData(String uri) {
+
+      BufferedReader reader = null;
 
       try {
-         response = client.execute(request);
-         return EntityUtils.toString(response.getEntity());
-      }catch (Exception e){
+         URL url = new URL(uri);
+         HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+         StringBuilder sb = new StringBuilder();
+         reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+         String line;
+         while ((line = reader.readLine()) != null) {
+            sb.append(line + "\n");
+         }
+
+         return sb.toString();
+
+      } catch (Exception e) {
          e.printStackTrace();
          return null;
-      }finally {
-         client.close();
+      } finally {
+         if (reader != null) {
+            try {
+               reader.close();
+            } catch (IOException e) {
+               e.printStackTrace();
+               return null;
+            }
+         }
       }
+
    }
+
 }
